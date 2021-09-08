f23ed497e43909321b25ecc23222b2ab79bd3e24
tongxingyu <tongxingyu@ln.chinamobile.com>
2021-09-08 17:17:46 +0800

    添加学习率调整策略。

--- train.py
+++ train.py
@@ -52,7 +52,14 @@ if __name__ == '__main__':
                                       batch_size=2,shuffle=False,drop_last=False,return_list=True)
 
     model = TopDown()
-    optimizer = paddle.optimizer.Adam(parameters=model.parameters(), learning_rate=5e-4)
+    learning_rate = paddle.optimizer.lr.MultiStepDecay(
+        learning_rate=5e-4, milestones=[170, 200], gamma=0.1)
+    lr = paddle.optimizer.lr.LinearWarmup(
+        learning_rate=learning_rate,
+        warmup_steps=500,
+        start_lr=0,
+        end_lr=5e-4)
+    optimizer = paddle.optimizer.Adam(parameters=model.parameters(), learning_rate=lr)
     max_epochs = 210
     epoch = 0
     best_mean = 0
@@ -64,8 +71,9 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
             model.clear_gradients()
+            lr.step()
             log_vars = output['log_vars']
-            print(f'epoch:{epoch} batch_id:{batch_id} loss:{log_vars["mse_loss"]} acc_pose:{log_vars["acc_pose"]}')
+            print(f'epoch:{epoch} batch_id:{batch_id} lr:{optimizer.get_lr()} loss:{log_vars["mse_loss"]} acc_pose:{log_vars["acc_pose"]}')
         i = 0
         results = []
         for data in val_loader:
